a0408cbb983a31f3df12efa8c5d71c189f56a096
Dan Halligan <dan.halligan@gmail.com>
2021-12-08 17:52:24 +0000

    Add template solutions

--- aoc2021/helpers.py
+++ aoc2021/helpers.py
@@ -1,6 +1,7 @@
 # Functions to help read inputs
 import re
 import os
+import requests
 
 def input_str(file):
     return open(file, 'r').read()
@@ -11,3 +12,11 @@ def input_lines(file):
 def input_ints(file):
     txt = input_str(file).rstrip()
     return list(map(int, re.split(r'[\n,]', txt)))
+
+def get_input(day):
+    res = requests.get(
+        f'https://adventofcode.com/2021/day/{day}/input',
+        cookies = {'session': os.environ.get('AOC_SESSION')}
+    )
+    with open("inputs/day" + f'{day:02d}' + ".txt", 'w') as f:
+        f.write(res.text)
