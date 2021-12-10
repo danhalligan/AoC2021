18df6e5b96c76c762a117555f84a1f5df1821368
Dan Halligan <dan.halligan@gmail.com>
2021-12-10 06:59:48 +0000

    Day 10

--- aoc2021/day09.py
+++ aoc2021/day09.py
@@ -1,41 +1,43 @@
 from aoc2021.helpers import *
 import numpy as np
+from itertools import product
 
 def data(file):
-    return np.array([list(map(int, list(x))) for x in input_lines(file)])
+    x = np.array([list(map(int, list(x))) for x in input_lines(file)])
+    return np.pad(x, 1, 'maximum')
 
 def neighbours(pos):
     i, j = pos
     return [(i-1,j), (i,j-1), (i+1,j), (i,j+1)]
 
+def array_centre(x):
+    rows, cols = np.shape(x)
+    return product(range(1, rows-1), range(1, cols-1))
+
 def part1(file):
-    dat = np.pad(data(file), 1, 'maximum')
+    x = data(file)
     sum = 0
-    for i in range(1, np.shape(dat)[0]-1):
-        for j in range(1, np.shape(dat)[1]-1):
-            nbs = [dat[x] for x in neighbours((i, j))]
-            if all(nbs > dat[i,j]):
-                sum += dat[i,j] + 1
+    for i, j in array_centre(x):
+        nbs = [x[nb] for nb in neighbours((i, j))]
+        if all(nbs > x[i,j]):
+            sum += x[i,j] + 1
     return sum
 
-def find_basin(dat, i, j):
+# flood fill from a given coordinate inside the 2D array
+def find_basin(x, i, j):
     basin = set({(i,j)})
-    nbs = [(i,j)]
-    while len(nbs) > 0:
-        curr = nbs.pop()
-        nbs += [x for x in neighbours(curr) if dat[x] < 9 and x not in basin]
-        basin = basin.union(set(nbs))
+    coords = [(i,j)]
+    while len(coords) > 0:
+        curr = coords.pop()
+        coords += [nb for nb in neighbours(curr) if x[nb] < 9 and nb not in basin]
+        basin = basin.union(set(coords))
     return basin
 
 def part2(file):
-    dat = np.pad(data(file), 1, 'maximum')
+    x = data(file)
     basins = []
-    for i in range(1, np.shape(dat)[0]-1):
-        for j in range(1, np.shape(dat)[1]-1):
-            if dat[i,j] < 9:
-                if any((i,j) in s for s in basins):
-                    continue
-                basins += [find_basin(dat, i, j)]
-
+    for i, j in array_centre(x):
+        if x[i,j] < 9 and not any((i,j) in s for s in basins):
+            basins += [find_basin(x, i, j)]
     top = sorted([len(x) for x in basins])[-3:]
     return np.prod(top)
