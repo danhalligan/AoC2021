b7611e3025eeec5e86ec256842b52501ac41d14f
Daniel Halligan <dan.halligan@fiosgenomics.com>
2021-12-10 06:59:48 +0000

    Day 10

--- aoc2021/day10.py
+++ aoc2021/day10.py
@@ -3,8 +3,45 @@ from aoc2021.helpers import *
 def data(file):
     return input_lines(file)
 
+def pair(c):
+    return {']':'[', ')':'(', '>':'<', '}':'{',
+            '[':']', '(':')', '<':'>', '{':'}'}[c]
+
+def score(c):
+    return {')':3, ']':57, '}':1197, '>':25137}[c]
+
 def part1(file):
-    return nil
+    total = 0
+    for line in data(file):
+        stack = []
+        for char in list(line):
+            error = False
+            if char in ['[', '(', '<', '{']:
+                stack += [pair(char)]
+            else:
+                if len(stack) == 0 or stack.pop() != char:
+                    total += score(char)
+                    continue
+    return total
+
+def score2(c):
+    return {')':1, ']':2, '}':3, '>':4}[c]
 
 def part2(file):
-    return nil
+    scores = []
+    for line in data(file):
+        stack = []
+        for char in list(line):
+            if char in ['[', '(', '<', '{']:
+                stack += [pair(char)]
+            else:
+                if len(stack) == 0 or stack.pop() != char:
+                    break
+        else:
+            ls = 0
+            for s in stack[::-1]:
+                ls = ls * 5 + score2(s)
+            scores += [ls]
+    scores.sort()
+    return scores[len(scores) // 2]
+
