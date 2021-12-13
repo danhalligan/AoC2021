41eb434869016d015b71aad120e25cf198e7315b
Dan Halligan <dan.halligan@gmail.com>
2021-12-13 17:32:13 +0000

    Simplification

--- aoc2021/day13.py
+++ aoc2021/day13.py
@@ -1,5 +1,6 @@
 from aoc2021.helpers import *
 import numpy as np
+from functools import reduce
 
 def data(file):
     dots, folds = input_str(file).rstrip().split("\n\n")
@@ -11,32 +12,26 @@ def data(file):
     return dots, folds
 
 def paper(dots):
-    size = (max(x[0] for x in dots)+1, max(x[1] for x in dots)+1)
+    size = (max(x[1] for x in dots)+1, max(x[0] for x in dots)+1)
     x = np.zeros(size)
-    for i, j in dots:
-        x[i,j] = 1
+    for i, j in dots: x[j, i] = 1
     return x
 
-def fold(x, i, axis):
+def fold(x, loc):
+    axis, i = loc
+    if axis == 'y':
+        return x[:i, :] + np.flip(x[i+1:, :], axis = 0)
     if axis == 'x':
-        x1 = x[0:i, ]
-        x2 = np.flip(x[(i+1):, ], 0)
-    else:
-        x1 = x[:,0:i]
-        x2 = np.flip(x[:, (i+1):], 1)
-    return np.add(x1, x2)
+        return x[:, :i] + np.flip(x[:, i+1:], axis = 1)
 
 def part1(file):
     dots, folds = data(file)
     x = paper(dots)
-    x = fold(x, folds[0][1], folds[0][0])
+    x = fold(x, folds[0])
     return sum(sum(x >= 1))
 
 def part2(file):
     dots, folds = data(file)
-    x = paper(dots)
-    for (axis, pos) in folds:
-        x = fold(x, pos, axis)
-    x = np.rot90(np.flip(x > 1, 1))
-    for r in x:
-        print(''.join({False:' ', True:'█'}[i] for i in r))
+    x = reduce(fold, folds, paper(dots)) >= 1
+    x = [''.join({False:' ', True:'█'}[i] for i in r) for r in x]
+    return "\n\n" + "\n".join(x)
