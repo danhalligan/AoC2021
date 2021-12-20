3dfab46cb8bbf3b7b8a984241cea76f6d57f8619
Dan Halligan <dan.halligan@gmail.com>
2021-12-20 10:18:38 +0000

    Day 20

--- aoc2021/day19.py
+++ aoc2021/day19.py
@@ -32,49 +32,47 @@ def rotations(p):
         (z, x, y)
     ]
 
-# @cache
 def rotate_scanner(x):
     return zip(*[rotations(c) for c in x])
 
-def find_match(s1, s2):
-    ref = np.array(s1)
-    refset = set([tuple(x) for x in ref])
-    s2 = tuple(tuple(x) for x in s2)
+def data(file):
+    dat = input_str(file)
+    chunks = dat.split("\n\n")
+    scanners = []
+    for chunk in chunks:
+        scanner = [tuple(map(int, l.split(','))) for l in chunk.splitlines()[1:]]
+        scanners += [scanner]
+    return scanners
+
+def find_match(refset, s2):
     for rx in rotate_scanner(s2):
-        rx = np.array(rx)
+        # rx = np.array(rx)
         for p2 in rx:
-            for p1 in ref:
-                diff = p1 - p2
-                tx = set([tuple(diff + x) for x in rx])
-                n = len(refset.intersection(tx))
-                if n >= 12:
-                    return refset.union(tx), diff
+            for p1 in refset:
+                diff = (p1[0]-p2[0], p1[1]-p2[1], p1[2]-p2[2])
+                count = 0
+                for x in rx:
+                    p  = (x[0]+diff[0], x[1]+diff[1], x[2]+diff[2])
+                    if p in refset: count += 1
+                    if count == 12:
+                        tx = set([(x[0]+diff[0], x[1]+diff[1], x[2]+diff[2]) for x in rx])
+                        return refset.union(tx), diff
     return None, None
 
-
-file = "inputs/day19.txt"
-dat = input_str(file)
-chunks = dat.split("\n\n")
-scanners = []
-for chunk in chunks:
-    scanner = [list(map(int, l.split(','))) for l in chunk.splitlines()[1:]]
-    scanners += [scanner]
-
-ref = scanners[0]
+file = "tests/inputs/day19.txt"
+scanners = data(file)
+ref = set(scanners[0])
 tosearch = set(range(1, len(scanners)))
 shifts = {}
-shifts[0] = np.array([0,0,0])
+shifts[0] = (0,0,0)
 while len(tosearch):
-    found = None
     for i in tosearch:
         tot, shift = find_match(ref, scanners[i])
-        if tot:
-            found = i
-            break
-    tosearch.remove(found)
+        if tot: break
+    tosearch.remove(i)
     shifts[i] = shift
-    ref = list(tot)
-    print(shift, i, len(ref))
+    ref = tot
+    print("matched", i)
 
 
 def part1(file):
@@ -84,6 +82,7 @@ def part2(file):
     best = 0
     for i in shifts.keys():
         for j in shifts.keys():
-            best = max(best, sum(abs(shifts[i] - shifts[j])))
+            x = shifts[i]
+            y = shifts[j]
+            best = max(best, sum(abs(x[i]-y[i]) for i in range(3)))
     return best
-
