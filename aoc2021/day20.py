73064c78f63815d71a7eee8db36eb5cd9a5127e1
Daniel Halligan <dan.halligan@fiosgenomics.com>
2021-12-20 10:18:38 +0000

    Day 20

--- aoc2021/day20.py
+++ aoc2021/day20.py
@@ -1,10 +1,60 @@
+from re import A
 from aoc2021.helpers import *
+from collections import defaultdict
+
+def as_int(x):
+    return [y == "#" for y in x]
 
 def data(file):
-    return input_lines(file)
+    dat = input_str(file)
+    chunks = dat.split("\n\n")
+    return as_int(chunks[0]), [as_int(x) for x in chunks[1].split('\n')]
 
+def neighbours(pos):
+    i,j = pos
+    return [
+        (i-1, j-1), (i, j-1), (i+1, j-1),
+        (i-1, j),   (i, j),   (i+1, j),
+        (i-1, j+1), (i,j+1),  (i+1, j+1)
+    ]
+
+def enhance(image, default, algorithm):
+    out = defaultdict(lambda: default)
+    r1 = min(x for x, y in image.keys()) - 2
+    r2 = max(x for x, y in image.keys()) + 2
+    c1 = min(y for x, y in image.keys()) - 2
+    c2 = max(y for x, y in image.keys()) + 2
+    for r in range(r1, r2):
+        for c in range(c1, c2):
+            pos = (c,r)
+            num = [str(int(image[p])) for p in neighbours(pos)]
+            num = int(''.join(num), 2)
+            out[pos] = algorithm[num]
+    return out
+
+def image_to_dict(x):
+    out = defaultdict(bool)
+    for r in range(len(x)):
+        for c in range(len(x[r])):
+            out[(r,c)] = x[c][r]
+    return out
+
+# This assumes that the algorithm converts the infinite dark pixels to light
+# and vice versa each iteration...
 def part1(file):
-    return nil
+    algorithm, image = data(file)
+    image = image_to_dict(image)
+    image = enhance(image, True, algorithm)
+    image = enhance(image, False, algorithm)
+    return sum(image.values())
 
 def part2(file):
-    return nil
+    algorithm, image = data(file)
+    image = image_to_dict(image)
+    for i in range(25):
+        image = enhance(image, True, algorithm)
+        image = enhance(image, False, algorithm)
+    return sum(image.values())
+
+
+
